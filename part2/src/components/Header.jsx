b5c47c478c24ea0e2c913d8980c604b4a7529256
Alain TRAN <alain.tran@opsone.net>
2024-01-18 11:24:00 +0100

    add 2.4 Course information step9 exercise

--- part2/src/components/Header.jsx
+++ part2/src/components/Header.jsx
@@ -1,3 +1,7 @@
-const Header = props => <h1 className="mb-6 text-3xl font-bold">{props.course}</h1>
+const Header = props => {
+  return(
+    <h1 className="mb-6 text-3xl font-bold">{props.courses}</h1>
+  )
+}
 
 export default Header
